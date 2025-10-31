5c4594ca0fadcb4f9364553e323dad69d0973535
Valentina Lopez <valentinalopro@unisabana.edu.co>
2025-10-31 08:31:27 -0500

    update app.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,4 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// Divide en dos entradas: core y react
+import * as ApolloCore from '@apollo/client';
+import * as ApolloReact from '@apollo/client/react';
+
+// Toma los símbolos de cada paquete correcto
+const { ApolloClient, InMemoryCache, HttpLink } = ApolloCore;
+const { ApolloProvider } = ApolloReact;
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: import.meta.env.VITE_GRAPHQL_URI || 'http://localhost:8080/graphql',
+  }),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
