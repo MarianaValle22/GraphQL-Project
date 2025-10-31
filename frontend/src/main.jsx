bdb5470dab522a7bbc9e4f2b7552e6763fcd5789
Valentina Lopez <valentinalopro@unisabana.edu.co>
2025-10-31 09:10:39 -0500

    update CAT_API_KEY

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-// Divide en dos entradas: core y react
+// Apollo: separa core y react para evitar conflictos de exports
 import * as ApolloCore from '@apollo/client';
 import * as ApolloReact from '@apollo/client/react';
 
-// Toma los símbolos de cada paquete correcto
 const { ApolloClient, InMemoryCache, HttpLink } = ApolloCore;
 const { ApolloProvider } = ApolloReact;
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: import.meta.env.VITE_GRAPHQL_URI || 'http://localhost:8080/graphql',
+    uri: 'https://graphql-backend-0fnu.onrender.com/graphql',
+    // Si tu backend usa cookies/sesión, descomenta:
+    // credentials: 'include',
   }),
   cache: new InMemoryCache(),
 });
@@ -22,4 +23,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </ApolloProvider>
 );
-
