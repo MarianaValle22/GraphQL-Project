bf3d0686cb5570f35947c079a4ddebc8660105a6
Valentina Lopez <valentinalopro@unisabana.edu.co>
2025-10-30 23:43:59 -0500

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-import * as Apollo from '@apollo/client';
-const { ApolloProvider } = Apollo;
-
-import { client } from './apollo/client';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
